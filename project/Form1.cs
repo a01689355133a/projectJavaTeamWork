3a4a3c1c575a633f42587562d21711769a9f081b
DUCDUNG <a01654464356@gmail.com>
2024-01-26 23:50:49 +0700

    confic

--- project/Form1.cs
+++ project/Form1.cs
@@ -16,5 +16,11 @@ namespace project
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //check
+            //dung
+        }
     }
 }
